5d7ef764c7af1d2d7fc1265d063e4145da86180f
Tarangladumor <tarangladumor@gmail.com>
2024-03-09 22:37:03 +0530

    3 schemas done

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,7 +1,44 @@
-const { default: mongoose } = require("mongoose");
+const {mongoose } = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    
+    firstName: {
+        type:String,
+        required : true
+    },
+    lastName : {
+        type:String,
+        required : true
+    }
+    ,
+    email : {
+        type : String,
+        required : true,
+    },
+    password: {
+        type : String,
+        required : true
+    },
+    accountType : {
+        type : String,
+        enum : ["Job Seeker","Employer"],
+        required:true,
+    },
+    token : {
+        type : String
+    },
+    jobs : {
+        type : mongoose.Schema.Types.Objectid,
+        ref : "Job"
+    },
+    jobSeeker : {
+        type : mongoose.Schema.Types.Objectid,
+        ref : "JobSeeker"
+    },
+    employer : {
+        type : mongoose.Schema.Types.Objectid,
+        ref : "Employer"
+    }
+
 })
 
 module.exports = mongoose.model("User", userSchema)
